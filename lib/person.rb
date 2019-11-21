47579b5f10b65d125268a1757dc16e0bff51d2e7
Eric Manning <emanningmusic@yahoo.com>
2019-11-21 05:12:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -10,6 +10,15 @@ wrist_size:, glove_size:, pant_length:, pant_width:)
    @birthday = birthday
    @hair_color = hair_color
    @eye_color = eye_color
+   @height = height
+   @weight = weight
+   @handed = handed
+   @complexion = complexion
+   @t_shirt_size = t_shirt_size
+   @wrist_size = wrist_size
+   @glove_size = glove_size
+   @pant_length = pant_length
+   @pant_width = pant_width
  end 
    
 end
