d21ae95a8227fea3ab9c3da593f8491999f5033c
Eric Manning <emanningmusic@yahoo.com>
2019-11-21 05:16:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,9 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def at
+def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
   
   #def initialize(name:, birthday:, hair_color:, eye_color:, height:, 
 #weight:, handed:, complexion:, t_shirt_size:, 
